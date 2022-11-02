8953fbdb146258e0e82f8a5ba7a277bd6628736e
Himanshu02kumar <113794542+Himanshu02kumar@users.noreply.github.com>
2022-11-02 10:37:40 +0530

    add
    add

---
+++ A.java
@@ -0,0 +1,14 @@
+class A{
+public static int sum(int a, int b){
+
+return a+b;
+}
+public static void main(String[] a){
+int x=20;
+int y=30;
+int c=sum(x, y);
+System.out.println("sum of "+x+" and "+y+" is: "+c);
+}
+}
+
+
